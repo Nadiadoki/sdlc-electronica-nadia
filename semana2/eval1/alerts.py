6443e14e75257f72065f2d818239a7640ef1f56b
Nadia <nadiadoki@gmail.com>
2026-07-25 17:13:51 -0600

    feat: implementar FileAlert (GREEN) - us-06

--- semana2/eval1/alerts.py
+++ semana2/eval1/alerts.py
@@ -11,3 +11,12 @@ class AlertStrategy(ABC):
 class ConsoleAlert(AlertStrategy):
     def send(self, message: str) -> None:
         print(message)
+
+
+class FileAlert(AlertStrategy):
+    def __init__(self, path: str) -> None:
+        self._path = path
+
+    def send(self, message: str) -> None:
+        with open(self._path, "a") as f:
+            f.write(message + "\n")
