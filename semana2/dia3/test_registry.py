5fa971fb93fb0fd1f2a4f1a260483807ff8b0ac9
Nadia <nadiadoki@gmail.com>
2026-07-24 23:02:45 -0600

    test: especificar SensorRegistry (RED) - us-01

---
+++ semana2/dia3/test_registry.py
@@ -0,0 +1,9 @@
+import pytest
+
+from registry import SensorRegistry, SensorNotFoundError
+
+
+def test_get_unknown_sensor_raises():
+    registry = SensorRegistry()
+    with pytest.raises(SensorNotFoundError):
+        registry.get("GHOST-99")
